08dfec3238005ed56b43e2ece69f1f9922b95204
malmosands <matthias@c1x.dev>
2021-06-10 10:40:39 +0200

    working update

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const { data, error } = useSWR('https://api.coingecko.com/api/v3/coins/categories', fetcher)
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  
+  if (data) return (
+    <>
+      <header className="flex justify-between items-center h-12 px-4">
+        <a href="#" className="text-white">
+          <span className="font-medium">altcoin</span>deck
         </a>
-      </footer>
-    </div>
+        <div>
+          <span>Last Update: {new Date().getTime()}</span>
+        </div>
+      </header>
+      <section className="grid grid-cols-3 gap-2 p-2">
+        {
+          data.map((cat, index) => (
+            <div key={index} className="transition bg-black bg-opacity-25 rounded border border-gray-800 hover:shadow-lg">
+              <header className="py-2 px-4 bg-black bg-opacity-25">
+                <h1 className="font-semibold text-white">{cat.name}</h1>
+              </header>
+              <div className="grid grid-cols-3 gap-2 py-2 px-4">
+                <div>
+                  <label className="text-xs">Market Cap</label>
+                  <span className="text-xs text-gray-200 truncate block">{cat.market_cap}</span>
+                </div>
+                <div>
+                  <label className="text-xs">Market Cap Change</label>
+                  <span className="text-xs text-gray-200 truncate block">{cat.market_cap_change_24h}</span>
+                </div>
+                <div>
+                  <label className="text-xs">Volume 24h</label>
+                  <span className="text-xs text-gray-200 truncate block">{cat.volume_24h}</span>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </section>
+    </>
   )
 }

743ecb5b0d1b87e109323812c035de87e70be8e6
malmosands <matthias@c1x.dev>
2022-02-17 17:06:42 +0100

    Update: Sidebar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -7,7 +7,7 @@ export default function Sidebar ({data}) {
   const [sortBy, setSortBy] = useState("TopGainers")
 
   return (
-    <div className="bg-black bg-opacity-50">
+    <div className="bg-black bg-opacity-30">
       <div className="grid grid-cols-2 font-medium">
         <button className={`focus:outline-none focus:text-white py-3 px-4 border-b-2 border-gray-800 hover:text-gray-200 ${sortBy == "TopGainers" && 'text-white border-teal-400'}`} onClick={() => setSortBy("TopGainers")}>Top <span className="font-semibold">Gainers</span></button>
         <button className={`focus:outline-none focus:text-white py-3 px-4 border-b-2 border-gray-800 hover:text-gray-200 ${sortBy == "TopLosers" && 'text-white border-red-400'}`} onClick={() => setSortBy("TopLosers")}>Top <span className="font-semibold">Losers</span></button>
